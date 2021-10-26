f3a4b2c742a21986decdf17b576b7a646aae311c
jiaolong1021 <shenjiaolong1021@163.com>
2021-10-26 10:38:21 +0800

    优化jenkins

--- src/views/config/jenkins.vue
+++ src/views/config/jenkins.vue
@@ -6,24 +6,8 @@
         <el-form-item label="地址">
           <el-input v-model="form.url" placeholder="请输入jenkins地址" />
         </el-form-item>
-        <el-form-item label="token">
-          <el-input v-model="form.token" />
-        </el-form-item>
         <el-form-item label="Job名称">
           <el-input v-model="form.job" />
-        </el-form-item>
-        <el-form-item label="分支">
-          <div class="zl-branch">
-            <el-row v-for="branch in branches" :key="branch" :gutter="10">
-              <el-col :span="14">
-                <el-input v-model="branch.name" placeholder="请输入分支名称" />
-              </el-col>
-              <el-col :span="8">
-                <el-button type="primary" size="mini" circle icon="el-icon-plus" @click="addBranch" />
-                <el-button v-if="branches.length > 1" type="primary" size="mini" circle icon="el-icon-minus" @click="minusBranch(branch)" />
-              </el-col>
-            </el-row>
-          </div>
         </el-form-item>
         <el-form-item label="">
           <el-button type="primary" size="mini" @click="save">保存</el-button>
@@ -33,16 +17,13 @@
   </div>
 </template>
 <script type="text/javascript">
-import { v4 as uuid } from 'uuid'
 export default {
   data() {
     return {
       form: {
         url: '',
-        token: '',
         job: ''
       },
-      branches: [],
       vscode: null
     }
   },
@@ -58,15 +39,10 @@ export default {
   },
   methods: {
     save() {
-      // 过滤空分支
-      this.branches = this.branches.filter((item) => {
-        return item.name
-      })
       this.vscode && this.vscode.postMessage({
         command: 'jenkinsSave',
         config: {
-          ...this.form,
-          branches: this.branches
+          ...this.form
         }
       })
     },
@@ -76,8 +52,6 @@ export default {
         case 'backJenkinsConfig':
           this.form.url = message.config.url
           this.form.job = message.config.job
-          this.form.token = message.config.token
-          this.branches = message.config.branches
           break
         case 'jenkinsSaveDone':
           this.$message({
@@ -88,19 +62,6 @@ export default {
         default:
           break
       }
-    },
-    // 添加分支
-    addBranch() {
-      this.branches.push({
-        id: uuid(),
-        name: ''
-      })
-    },
-    // 减少分支
-    minusBranch(branch) {
-      this.branches = this.branches.filter((item) => {
-        return item.id !== branch.id
-      })
     }
   }
 }
